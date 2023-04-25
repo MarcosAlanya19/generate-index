e422f18cc165c367a5dc11bf35f787586e32c9cc
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2023-04-25 13:28:45 -0500

    UPDATE: generate

--- generate.js
+++ generate.js
@@ -1,32 +1,27 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
-// eslint-disable-next-line no-undef
-// Toma el tercer elemento de process.argv como la ruta de la carpeta
-const directoryPath = process.argv[2];
+function generateIndex(dirPath) {
+  const files = fs.readdirSync(dirPath);
+  const allowedExtensions = ['.ts', '.tsx', '.jsx', '.js'];
 
-fs.readdir(directoryPath, (err, files) => {
-  // msg en caso la carpeta no exista
-  if (err) {
-    console.log(`Error al leer la carpeta ${directoryPath}: ${err}`);
-    return;
-  }
-
-  // verifica los archivos que acaben en js jsx ts tsx
-  const exportStatements = files
-    .filter((file) => file.match(/\.jsx?$|\.tsx?$/))
-    .map((file) => `export * from './${file.replace(/\.(jsx?|tsx?)$/, '')}';`)
+  const indexContents = files
+    .filter((file) => {
+      const extension = path.extname(file);
+      const name = path.parse(file).name;
+      return allowedExtensions.includes(extension) && name !== 'index';
+    })
+    .map((file) => {
+      const name = path.parse(file).name;
+      return `export * from './${name}'`;
+    })
     .join('\n');
 
+  fs.writeFileSync(path.join(dirPath, 'index.ts'), indexContents);
+  console.log('Creado exitosamente');
+}
 
-  const indexFileContent = `${exportStatements}\n`;
-
-  // genera el index.ts
-  fs.writeFile(`${directoryPath}/index.ts`, indexFileContent, (err) => {
-    if (err) {
-      console.log(`Error al escribir el archivo index.ts: ${err}`);
-      return;
-    }
-
-    console.log(`El archivo index.ts ha sido creado en la carpeta ${directoryPath}`);
-  });
-});
+// eslint-disable-next-line no-undef
+const args = process.argv.slice(2);
+const dirPath = args[0];
+generateIndex(dirPath);
